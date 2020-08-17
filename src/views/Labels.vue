e8cd25801724a6874b6a7fbcd77851ac62aab007
whj <739960552@qq.com>
2020-08-17 13:36:25 +0800

    Nav

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     Label.vue
+    <Nav />
   </div>
 </template>
 
 <script lang="ts">
   export default {
-    name: 'Labels'
+    name: 'Labels',
   };
 </script>
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     Money.vue
+    <Nav />
   </div>
 </template>
 
 <script lang="ts">
   export default {
-    name: 'Money'
+    name: 'Money',
   };
 </script>
 
