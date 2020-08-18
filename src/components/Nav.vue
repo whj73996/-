ccaa45198081e9360a5122c2bc5eb3fa2108faad
whj <739960552@qq.com>
2020-08-18 17:57:29 +0800

    1.router-link active-class 2.taobao meta 3.scss全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,49 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="cash"/>
+      花费
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="data"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="label"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
+
+  import Icon from '@/components/Icon.vue';
   export default {
-    name: 'Nav'
+    name: 'Nav',
+    components: {Icon}
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px red solid;
+  @import "~@/assets/style/helper.scss";
+
+  nav{
+    display: flex;
+    box-shadow:0 -2px 6px rgba(0,0,0,0.1);
+    > .item{
+      padding: 4px 0;
+      width: 33.33%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      >.icon{
+        width: 32px;
+        height: 32px;
+      }
+    }
+    >.item.selected{
+      color:$color-highlight;
+    }
   }
 </style>
