4fc75d05671c5c73b771ff63a841b2e58faec8df
whj <739960552@qq.com>
2020-08-19 15:56:36 +0800

    bug

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type==='-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type==='+' && 'selected'" @click="selectType('+')" >收入</li>
+    </ul>
+  </div>
+</template>
+
+<script >
+  console.log('11111111')
+  export default {
+    name: 'Types',
+    data(){
+      return{
+        type:'-'//减号表示支出，加好表示收入
+      }
+    },
+    methods:{
+      selectType(type){
+        console.log('000')
+        if(type!=='-'&&type!=='+'){
+          throw new Error('type is unknown')
+        }
+        this.type =type
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+</style>
