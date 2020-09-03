663c97203b3951916e0a452c60d30ac2edea17e1
whj <739960552@qq.com>
2020-09-03 22:25:04 +0800

    alert弹窗暴露地址

---
+++ src/components/Money/Nm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Nm extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
